ac011acd36dae79b6ad9d26948a1d18954dec221
Chao Gong <>
2023-12-28 09:41:46 +1300

    always start of month doing monthly top up

--- src/app/api/topup-month/route.ts
+++ src/app/api/topup-month/route.ts
@@ -1,17 +1,26 @@
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 import { prisma } from '../../../../prisma/prisma';
+import dayjs from 'dayjs';
 
 const patchHandler = async () => {
   try {
     const session = await getSession();
     const user = session?.user;
     const forecasts = await prisma.forecast.findMany();
+
+    const now = dayjs();
+    const currentDate = now.date();
+    const updateDate =
+      currentDate >= 15
+        ? now.startOf('month').add(1, 'month').toDate()
+        : now.startOf('month').toDate();
+
     if (forecasts && user) {
       const events = forecasts.map((forecast) => {
         return {
           action: 'topup',
           amount: forecast.forecast,
-          create_at: new Date(),
+          create_at: updateDate,
           user: user.name,
           comments: null,
           forecast_id: forecast.id,
@@ -26,8 +35,8 @@ const patchHandler = async () => {
             ...forecast,
             balance: forecast.balance.add(forecast.forecast),
             last_topup: forecast.forecast,
-            last_topup_at: new Date(),
-            update_at: new Date(),
+            last_topup_at: updateDate,
+            update_at: updateDate,
           };
 
           await prisma.forecast.update({
